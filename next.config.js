878b59fa775c03c06fcecba196103a7bda642a62
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-16 15:18:42 +0530

    update 0n 16 feb

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === "development",
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ postcss.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-nesting': {},
+    'tailwindcss': {},
+    'autoprefixer': {}
+  },
+};
+
+export default config;
