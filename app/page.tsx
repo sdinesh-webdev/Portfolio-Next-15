5103ab0b3011c0a951d7d08d768094d5148d4d4b
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-18 17:43:56 +0530

    uiux fixes

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       syncTouch: true, // Enable smooth scrolling on touch devices
-      touchMultiplier: 1,
+      touchMultiplier: 2,
     });
 
     function raf(time: number) {
