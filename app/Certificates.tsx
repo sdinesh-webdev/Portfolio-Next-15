89ab9f4c454b8ce65a6917b34ed6a793c2affdbb
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-18 14:13:12 +0530

    brake img fix

--- app/Certificates.tsx
+++ app/Certificates.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import useGsapReveal from './useGsapReveal';
 import Work from './Work';
 import './globals.css';
--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: false,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
