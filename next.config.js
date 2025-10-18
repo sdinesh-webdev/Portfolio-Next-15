a45282e47ec9c65000cfb387916bd6b5876bfa89
S DINESH KUMAR <jhmuk7899@gmail.com>
2025-10-18 15:49:21 +0530

    Enable SWC minification in Next.js config

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  swcMinify: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
